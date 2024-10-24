c60e53a56191feda4f5ed8d90a3cd481c37c546b
AriaGomes <aria.gomes00@gmail.com>
2024-10-24 08:15:32 -0400

    Working PoC

--- app/components/downloadWidget.tsx
+++ app/components/downloadWidget.tsx
@@ -8,19 +8,39 @@ export const DownloadWidget = ({ selected, setSelected }: { selected: Package[],
     
     const [command, setCommand] = useState<string | null>(null);
 
-    useEffect(() => {
-        console.log(command);
-    }, [command]);
-
-
-    const handleClick = () => {
-        setCommand(getCommand(selected))
+    const handleCopyClick = () => {
         navigator.clipboard.writeText(command || "");
     };
+
+    useEffect(() => {
+        setCommand(getCommand(selected));
+    }, [selected]); 
+
+    const handleDownloadClick = () => {
+        if (command) {
+            const blob = new Blob([command], { type: "text/plain" });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.href = url;
+            a.download = "install.ps1";
+            a.click();
+            URL.revokeObjectURL(url);
+        } else {
+            console.error("Command is null");
+        }
+    };
+    
     return (
         <div className="fixed bottom-4 right-4 bg-white border border-gray-300 p-4 shadow-lg z-50">
-          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700" onClick={handleClick}>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700" onClick={() => {
+      
+            handleDownloadClick()}}>
             Download
+          </button>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700" onClick={()=> {
+  
+            handleCopyClick()}}>
+            Copy
           </button>
         </div>
       );
