c4384e37457a8b717ef7c48f24f3ac792080d676
AriaGomes <aria.gomes00@gmail.com>
2024-11-26 18:48:55 -0500

    downloadInc plan

--- app/api/downloadIncrement/[id]/route.tsx
+++ app/api/downloadIncrement/[id]/route.tsx
@@ -1,1 +1,7 @@
 // When a user copies or downloads their selection increment the download count in the app list json file
+
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+    return NextResponse.json(req);
+};
