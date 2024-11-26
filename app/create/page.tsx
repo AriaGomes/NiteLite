c92448d1d2bc689ccddccfe30ff6d66bddc1e7ff
AriaGomes <aria.gomes00@gmail.com>
2024-11-26 18:51:42 -0500

    create -> createPage

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
-export const Create = () => {
-    return(
-    <div className="text-white p-10">
-        Create
-    </div>
-    )
-}
+const CreatePage = () => {
+    return (
+        <div className="text-white p-10">
+            Create
+        </div>
+    );
+};
 
-export default Create;
+export default CreatePage;
